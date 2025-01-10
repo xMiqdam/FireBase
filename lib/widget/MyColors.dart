a8cdd37afb48219f1cf6a7261aa731b242fcf9dc
KaIkaf <caesarkai872@gmail.com>
2025-01-10 16:54:13 +0700

    Feat:[CK] Menambah widget MyColors

---
+++ lib/widget/MyColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const lightBeige = Color(0xFFF5ECD5); 
+const tealGreen = Color(0xFF578E7E);  
+const brightRed = Color(0xFFE33629); 
+const blackz = Color(0xFF000000);
+const redColor = Color(0xFFFF0000);
