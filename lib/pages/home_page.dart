a651d9b6b1e264f375c7b5f3d6c2babec0b8ca7e
Rakha Ghiyas Arrahman <yibi9817@gmail.com>
2025-01-18 12:07:01 +0700

    feat [RG] feature create and read

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:mynoteapps/api/firebase_api.dart';
 import 'login_page.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final FirebaseApi firebaseapi = FirebaseApi();
+
+  final TextEditingController textController = TextEditingController();
+
   Future<void> signOut(BuildContext context) async {
     try {
     
@@ -22,6 +35,23 @@ class HomePage extends StatelessWidget {
     }
   }
 
+  void openNoteBox(){
+    showDialog(context: context, builder: (context) => AlertDialog(
+      content: TextField(
+        controller: textController,
+      ),
+      actions: [
+        ElevatedButton(onPressed: () {
+          firebaseapi.addNote(textController.text);
+
+          textController.clear();
+
+          Navigator.pop(context);
+        }, child: Text("Add"))
+      ],
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,12 +66,31 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: TextStyle(fontSize: 24),
+      floatingActionButton: FloatingActionButton(
+        onPressed: openNoteBox,
+        child: const Icon(Icons.add),
         ),
-      ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: firebaseapi.getNotesStream(),
+          builder: (context, snapshot) {
+            if(snapshot.hasData){
+              List notesList = snapshot.data!.docs;
+              return ListView.builder(
+                itemCount: notesList.length,
+                itemBuilder: (context, index) {
+                DocumentSnapshot document = notesList[index];
+                String docID = document.id;
+                Map<String, dynamic> data = document.data() as Map<String,dynamic>;
+                String noteText = data['note'];
+                return ListTile(
+                  title: Text(noteText),
+                );
+              },);
+            }else{
+              return const Text( "no notes");
+            }
+          },
+        ),
     );
   }
 }
